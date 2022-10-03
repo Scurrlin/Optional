47abe844335b277c2861dd49c469bcfeecd5a128
Scurrlin <109078639+Scurrlin@users.noreply.github.com>
2022-10-03 09:23:41 -0400

    server update

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const favicon = require('serve-favicon');
 require('dotenv').config();
 require('./config/database');
 
+const User = require('./models/user');
+
 let u, i, c, o;
 
 const app = express();
