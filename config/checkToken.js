45f362ce9e9ee304f5ea7f91cab3d4c5c1a67d1c
Scurrlin <109078639+Scurrlin@users.noreply.github.com>
2022-10-03 11:34:24 -0400

    database debug

--- config/checkToken.js
+++ config/checkToken.js
@@ -1,51 +1,23 @@
-// const jwt = require('jsonwebtoken');
-// const SECRET = process.env.SECRET;
-
-// module.exports = function (req, res, next) {
-// 	// Check for the token being sent in a header or as a query parameter
-// 	let token = req.get('Authorization') || req.query.token;
-// 	if (token) {
-// 		// Remove the 'Bearer ' if it was included in the token header
-// 		token = token.replace('Bearer ', '');
-// 		// Check if the token is valid and not expired
-// 		jwt.check(token, process.env.SECRET, function (err, decoded) {
-// 			// If valid token, decoded will be the token's entire payload
-// 			// If invalid token, err will be set
-// 			req.user = err ? null : decoded.user;
-// 			// If your app cares...(optional)
-// 			req.exp = err ? null : new Date(decoded.exp * 1000);
-// 			return next();
-// 		});
-// 	} else {
-// 		// No token was sent
-// 		req.user = null;
-// 		return next();
-// 	}
-// };
-
 const jwt = require('jsonwebtoken');
-const SECRET = process.env.SECRET;
 
-module.exports = function(req, res, next) {
-  // Check for the token being sent in three different ways
-  let token = req.get('Authorization') || req.query.token || req.body.token;
-  if (token) {
-    // Remove the 'Bearer ' if it was included in the token header
-    token = token.replace('Bearer ', '');
-    // Check if token is valid and not expired
-    jwt.verify(token, SECRET, function(err, decoded) {
-      if (err) {
-        req.user = null;
-        return next();
-      } else {
-         // If invalid token, err will be set
-        req.user = decoded.user 
-        return next(); // passes the req object to the next place in the middleware chain!
-      }
-    });
-  } else {
-    // not token
-    req.user = null;
-    next();
-  }
+module.exports = function (req, res, next) {
+	// Check for the token being sent in a header or as a query parameter
+	let token = req.get('Authorization') || req.query.token;
+	if (token) {
+		// Remove the 'Bearer ' if it was included in the token header
+		token = token.replace('Bearer ', '');
+		// Check if the token is valid and not expired
+		jwt.check(token, process.env.SECRET, function (err, decoded) {
+			// If valid token, decoded will be the token's entire payload
+			// If invalid token, err will be set
+			req.user = err ? null : decoded.user;
+			// If your app cares...(optional)
+			req.exp = err ? null : new Date(decoded.exp * 1000);
+			return next();
+		});
+	} else {
+		// No token was sent
+		req.user = null;
+		return next();
+	}
 };
--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.DATABASE_URL // < replace with your database name!
-);
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+});
 
 const db = mongoose.connection;
 
 db.on('connected', function () {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+	console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
